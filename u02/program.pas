f1bba7a80d7cae4e5248bc71e28c72c31b8a47d2
Václav <vaclav.volhejn@gmail.com>
2018-03-01 20:38:27 +0100

    Assignment 2

---
+++ u02/program.pas
@@ -0,0 +1,11 @@
+begin
+    WriteLn('Foo');
+    WriteLn('12 45');
+    WriteLn('12 ctyricetpet');
+    WriteLn('1');
+    WriteLn('2 2');
+    WriteLn('3 3 3');
+    WriteLn('4 4 4 4');
+    WriteLn('5 5 5 5 5');
+    WriteLn('Bar');
+end.
